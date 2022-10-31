81d3987c848689ff225696ffe59be2dead0ebd3b
Nicolas-Verbaere <nverbaere@gmail.com>
2022-10-31 16:59:00 +0100

    created server

---
+++ public/js/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = require("./router");
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static("public"));
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`server listening at http://localhost:${port}`);
+});
