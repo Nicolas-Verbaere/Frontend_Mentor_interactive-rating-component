0bcf9f4bb12bb878795c2147365ea8577295f957
Nicolas-Verbaere <nverbaere@gmail.com>
2022-11-20 16:35:46 +0100

    SubmitButton ok

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
-const buttonSubmit = document.getElementById('buttonSubmit')
-// buttonSubmit.addEventListener('click', function(){
-//     alert('bonjour')
-// })
-console.log(buttonSubmit)
+document.addEventListener("DOMContentLoaded", function(event) {
+    const buttonSubmit = document.getElementById('buttonSubmit')
+    buttonSubmit.addEventListener('click',function(event) {
+        console.log(event)
+        console.log('click')   
+    })
+});
