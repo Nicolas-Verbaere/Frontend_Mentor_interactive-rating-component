97edb07111ef19c96a5a26e8da721f86fab14424
Nicolas-Verbaere <nverbaere@gmail.com>
2022-12-15 16:28:19 +0100

    buttons ok and change page ok

---
+++ back/index.js
@@ -0,0 +1,31 @@
+const button1 = document.querySelector('.button1');
+button1.addEventListener('click', event => {
+      console.log('click button1')
+});
+
+const button2 = document.querySelector('.button2');
+button2.addEventListener('click', event => {
+      console.log('click button2')
+});
+
+const button3 = document.querySelector('.button3');
+button3.addEventListener('click', event => {
+      console.log('click button3')
+});
+
+const button4 = document.querySelector('.button4');
+button4.addEventListener('click', event => {
+      console.log('click button4')
+});
+
+const button5 = document.querySelector('.button5');
+button5.addEventListener('click', event => {
+      console.log('click button5')
+});
+
+
+const buttonSubmit = document.querySelector('.buttonSubmit');
+buttonSubmit.addEventListener('click', event => {
+      // buttonSubmit.innerHTML = `Nombre de clics : ${event.detail}`;
+      console.log('click')
+});
--- public/js/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    const buttonSubmit = document.getElementById('buttonSubmit')
-    buttonSubmit.addEventListener('click',function(event) {
-        console.log(event)
-        console.log('click')   
-    })
-});
