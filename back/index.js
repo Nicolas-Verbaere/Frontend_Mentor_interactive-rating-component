84d9c8e3cf0967a9c1e5dacacbb596f226b87348
Nicolas-Verbaere <nverbaere@gmail.com>
2022-12-15 17:23:42 +0100

    create form nok

--- back/index.js
+++ back/index.js
@@ -1,26 +1,31 @@
 const button1 = document.querySelector('.button1');
 button1.addEventListener('click', event => {
-      console.log('click button1')
+      console.log('click button1');
+      event.preventDefault();
 });
 
 const button2 = document.querySelector('.button2');
 button2.addEventListener('click', event => {
-      console.log('click button2')
+      console.log('click button2');
+      event.preventDefault();
 });
 
 const button3 = document.querySelector('.button3');
 button3.addEventListener('click', event => {
-      console.log('click button3')
+      console.log('click button3');
+      event.preventDefault();
 });
 
 const button4 = document.querySelector('.button4');
 button4.addEventListener('click', event => {
-      console.log('click button4')
+      console.log('click button4');
+      event.preventDefault();
 });
 
 const button5 = document.querySelector('.button5');
 button5.addEventListener('click', event => {
-      console.log('click button5')
+      console.log('click button5');
+      event.preventDefault();
 });
 
 
