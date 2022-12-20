42b4294ad6b337a4438fbd6953da7312729b84bd
Nicolas-Verbaere <nverbaere@gmail.com>
2022-12-20 11:34:28 +0100

    selected button and read value button1

--- back/app.js
+++ back/app.js
@@ -1,31 +1,54 @@
+function getValue() {
+      // Sélectionner l'élément input et récupérer sa valeur
+      let input = document.querySelector(".button").value;
+      // Afficher la valeur
+      console.log("valeur de l'input: ", input);
+  }
+
+
 const button1 = document.querySelector('.button1');
 button1.addEventListener('click', event => {
-      console.log('click button1');
       event.preventDefault();
+      console.log('click button1');
+
+      const value1 = event.target;
+      console.log(value1)
 });
 
 const button2 = document.querySelector('.button2');
 button2.addEventListener('click', event => {
-      console.log('click button2');
       event.preventDefault();
+      console.log('click button2');
+
+      const value2 = event.target;
+      console.log(value2)
 });
 
 const button3 = document.querySelector('.button3');
 button3.addEventListener('click', event => {
-      console.log('click button3');
       event.preventDefault();
+      console.log('click button3');
+
+      const value3 = event.target;
+      console.log(value3)
 });
 
 const button4 = document.querySelector('.button4');
 button4.addEventListener('click', event => {
-      console.log('click button4');
       event.preventDefault();
+      console.log('click button4');
+
+      const value4 = event.target;
+      console.log(value4)
 });
 
 const button5 = document.querySelector('.button5');
 button5.addEventListener('click', event => {
-      console.log('click button5');
       event.preventDefault();
+      console.log('click button5');
+
+      const value5 = event.target;
+      console.log(value5)
 });
 
 
